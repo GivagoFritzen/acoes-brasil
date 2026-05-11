5898e9959cd017b9ceed5d8b3cc7cc0d0e52e9e8
GivagoFritzen <givago.fritzen@hotmail.com>
2026-05-11 19:29:00 -0300

    Ajuste sonar

--- back/src/config/env.ts
+++ back/src/config/env.ts
@@ -8,7 +8,7 @@ export const env = {
     host: process.env.DB_HOST ?? "localhost",
     name: process.env.DB_NAME ?? "AppDb",
     user: process.env.DB_USER ?? "sa",
-    password: process.env.DB_PASSWORD ?? "Senha@123",
+    password: process.env.DB_PASSWORD ?? "",
     dialect: (process.env.DB_DIALECT ?? "mssql") as "mssql" | "mysql" | "sqlite",
     storage: process.env.DB_STORAGE,
   },
